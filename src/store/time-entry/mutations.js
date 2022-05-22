fac61c801f893db11241fe0df1e59bf101fa4afe
Kristian <kristian383@gmail.com>
2022-05-22 21:10:55 +0200

    Cleaned comments

--- src/store/time-entry/mutations.js
+++ src/store/time-entry/mutations.js
@@ -4,19 +4,15 @@ export default {
 
         for (let i = 0; i < payload.length; i++) {
             const element = payload[i];
-            // console.log(element)
             const service = state.services.find(serv => {
                 return serv.id == element.relationships.service.data.id
             });
             element.attributes.service_name = service.attributes.name;
             state.timeEntries.push(element)
         }
-
-        // console.log(state.timeEntries)
     },
     setServices(state, payload) {
         state.services = payload
-        // console.log(state.services)
     },
     deleteTimeEntry(state, id) {
         let index = state.timeEntries.findIndex(entry => entry.id === id)
@@ -26,7 +22,6 @@ export default {
     },
     editTimeEntry(state, payload) {
         let index = state.timeEntries.findIndex(entry => entry.id === payload.id)
-        console.log(index)
         state.timeEntries[index] = payload;
     }
 }
